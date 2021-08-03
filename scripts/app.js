5d26c7de9113fa8166d2042d5cbacdeb3d7e2738
maass78 <anden.tolm@gmail.com>
2021-08-03 15:20:48 +0300

    Сделал вывод результата

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,76 @@
 const inputFile = document.getElementById('inputImage');
+const checkboxResize = document.getElementById('checkboxResize');
+const inputImageWidth = document.getElementById('inputImageWidth');
+const inputImageHeight = document.getElementById('inputImageHeight');
+const imageSizeContainer = document.getElementById('imageSizeContainer');
+const radioSymbolsButtons = document.getElementsByName('symbols');
+const resultText = document.getElementById('resultText');
+const resultWrapper = document.getElementById('resultWrapper');
+const buttonPlusSize = document.getElementById('buttonPlusSize');
+const buttonMinusSize = document.getElementById('buttonMinusSize');
+const buttonCopyToClipboard = document.getElementById('buttonCopyToClipboard');
 
-const imageWidht = 200;
-const imageHeight = 200;
+buttonPlusSize.addEventListener('click', onResultSizePlus);
+buttonMinusSize.addEventListener('click', onResultSizeMinus);
+buttonCopyToClipboard.addEventListener('click', onResultCopyButtonClicked);
 
 inputFile.addEventListener('change', onInputFileChanged);
+checkboxResize.addEventListener('change', onCheckboxResizeChanged);
+
+function onResultCopyButtonClicked() {
+    copyToClipboard(resultText.innerText);
+}
+
+function copyToClipboard(text) {
+    if (window.clipboardData && window.clipboardData.setData) {
+        return window.clipboardData.setData("Text", text);
+
+    }
+    else if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
+        var textarea = document.createElement("textarea");
+        textarea.textContent = text;
+        textarea.style.position = "fixed"; 
+        document.body.appendChild(textarea);
+        textarea.select();
+        try {
+            return document.execCommand("copy");
+        }
+        catch (ex) {
+            console.warn("Copy to clipboard failed.", ex);
+            return false;
+        }
+        finally {
+            document.body.removeChild(textarea);
+        }
+    }
+}
+
+
+function onCheckboxResizeChanged(event) {
+    inputImageWidth.disabled = !checkboxResize.checked;
+    inputImageHeight.disabled = !checkboxResize.checked;
+    imageSizeContainer.style.color = checkboxResize.checked ? '#ffffff' : '#808080';
+}
 
 function onInputFileChanged(event) {
+
+    if(checkboxResize.checked) {
+
+        if(!inputImageWidth.value 
+            || !inputImageHeight.value 
+            || inputImageWidth.value < 1 
+            || inputImageHeight.value < 1)
+        {
+            window.alert('Введите размер изображения, раз уж решили изменять его!');
+            return;
+        }
+    }
+
+    if(!this.files)
+        return;
+
+    const imageWidth = inputImageWidth.value;
+    const imageHeight = inputImageHeight.value;
 
     let file = this.files[0];
     let fileReader = new FileReader();
@@ -14,9 +79,10 @@ function onInputFileChanged(event) {
     const ctx = cvs.getContext('2d');
 
     let img = new Image;
+    let selectedSymbols = getSelectedRadio(radioSymbolsButtons);
     img.onload = function() {
 
-        cvs.width = imageWidht;
+        cvs.width = imageWidth;
         cvs.height = imageHeight;
         ctx.drawImage(img, 0, 0, cvs.width, cvs.height);
 
@@ -29,12 +95,14 @@ function onInputFileChanged(event) {
                  
                 let rgbPixel = getPixelXY(imageData, x, y);
                 
-                drawableString.setPixel(x, y, getCharByColor(rgbPixel[0], rgbPixel[1], rgbPixel[2]));
+                let char = getCharByColor(rgbPixel[0], rgbPixel[1], rgbPixel[2], selectedSymbols);
+
+                drawableString.setPixel(x, y, char);
             }
            
         }
-
-        console.log(drawableString.toString());
+        resultWrapper.style.display = 'block';
+        resultText.innerText = drawableString.toString();
     }
 
     fileReader.onload = function() {
@@ -49,6 +117,29 @@ function onInputFileChanged(event) {
     fileReader.readAsDataURL(file);
 }
 
+function onResultSizePlus(event) {
+
+    let currentSize = resultText.style.fontSize.replace('px', '').replace('pt', '') * 1;
+    currentSize++;
+    resultText.style.fontSize = `${currentSize}px`;
+}
+
+function onResultSizeMinus(event) {
+    let currentSize = resultText.style.fontSize.replace('px', '').replace('pt', '') * 1;
+    currentSize--;
+
+    if(currentSize > 0)
+        resultText.style.fontSize = `${currentSize}px`;
+}
+
+function getSelectedRadio(radioButtons){
+    for (let i = 0; i < radioButtons.length; i++) {
+        if(radioButtons[i].checked){
+            return radioButtons[i].value;        
+        }
+    }
+}
+
 function getPixel(imageData, index) {
     let i = index * 4, d = imageData.data;
     return [d[i], d[i + 1], d[i + 2], d[i + 3]] 
@@ -58,18 +149,15 @@ function getPixelXY(imageData, x, y) {
     return getPixel(imageData, y * imageData.width + x);
 }
 
-function getCharByColor(r, g, b) {
+function getCharByColor(r, g, b, symbolsType) {
     
     let brightness = (r + g + b) / 3;
 
-    if (brightness >= 0 && brightness < 50)
-        return '█';
-    else if (brightness >= 50 && brightness < 120)
-        return '▓';
-    else if (brightness >= 120 && brightness < 180)
-        return '▒';
-    else
-        return '░';
+    let symbols = symbolsType === 'ascii' ? ['@', '#', 'S', '%', '?', '*', '+', ':', ',', '.'] : ['█', '▓', '▒', '░'] ;//['.', ',', ':', '+', '*', '?', '%', 'S', '#', '@'] : ['░', '▒', '▓', '█'];
+    let step = (brightness / 255) * (symbols.length - 1);
+
+    let index = Math.floor(step);
+    return symbols[index];
 }
 
 class DrawableString {
@@ -106,7 +194,7 @@ class DrawableString {
             }
             output += '\n';
         }
-
+        
         return output;
     }
 }
